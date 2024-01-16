9e78658f4821205f990f2c32990af6e5e41ce52e
nakko <langonebros2@gmail.com>
2024-01-17 00:24:30 +0100

    did barebones

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,15 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import Card from './lib/CentralCard.svelte'
 </script>
 
 <main>
-
+  
 </main>
-
+  <Card />
 <style>
-
+  :root{
+    background-color: #a8a7a7;
+  }
 </style>
---
+++ src/lib/CentralCard.svelte
@@ -0,0 +1,49 @@
+<script>
+    let paragraphs = [
+        {
+            title: "Rappresentanza Studenti",
+            p: "La Consulta Provinciale degli Studenti di Firenze è un organismo rappresentativo che svolge un ruolo fondamentale nell'ambito della partecipazione studentesca e nella promozione degli interessi degli studenti a livello locale. Fondata con l'obiettivo di favorire il coinvolgimento attivo degli studenti nelle decisioni che riguardano il mondo scolastico e universitario, la Consulta funge da ponte tra gli studenti e le istituzioni educative.",
+        },
+        {
+            title: "Partecipazione Attiva",
+            p: "Composta da rappresentanti eletti dagli studenti stessi, la Consulta Provinciale di Firenze si impegna a rappresentare in modo efficace le varie esigenze e opinioni degli studenti, contribuendo così a creare un dialogo costruttivo con le istituzioni scolastiche, universitarie e locali. Attraverso la partecipazione attiva, la Consulta cerca di affrontare le problematiche quotidiane che gli studenti possono incontrare nel loro percorso educativo, promuovendo al contempo iniziative e progetti che possano migliorare la qualità dell'istruzione e della vita studentesca.",
+        },
+        {
+            title: "Iniziative Studentesche",
+            p: "Oltre a svolgere un ruolo di advocacy, la Consulta Provinciale degli Studenti di Firenze organizza anche eventi, incontri e campagne informative, al fine di sensibilizzare la comunità studentesca su questioni rilevanti e incoraggiare la partecipazione attiva. In questo modo, la Consulta diventa un punto di riferimento per gli studenti che desiderano essere coinvolti nella costruzione di un ambiente educativo più inclusivo, equo e partecipativo.",
+        },
+    ];
+
+    import { fade } from 'svelte/transition';
+    
+    let isVisible = false;
+
+    function fadeIn(){
+        isVisible = true;
+    }
+</script>
+
+
+<div
+    class="min-h-screen xl:w-2/3 lg:w-3/4 sm:w-5/6 mx-auto sm:my-2 bg-[#fffbff] sm:rounded-3xl"
+>
+    <p class="md:text-5xl sm:text-4xl text-3xl font-bold text-center pt-5">
+        Consulta Provinciale Degli Studenti Firenze
+    </p>
+
+    <div class="flex justify-center my-5 mx-2">
+        <img
+            src="images/palazzo-del-pegaso-via-cavour-firenze.jpg"
+            alt="foto palazzo pegaso"
+            class="rounded-3xl sm:w-5/6"
+        />
+    </div>
+    <div class="grid grid-cols-1 place-items-center mx-2 sm:mx-0">
+        {#each paragraphs as { title, p }}
+            <div class="sm:w-5/6 text-center bg-[#e2e2e2] my-5 px-2 rounded-xl">
+                <p class="text-2xl font-[600] text-start my-2">{title}</p>
+                <p>{p}</p>
+            </div>
+        {/each}
+    </div>
+</div>
