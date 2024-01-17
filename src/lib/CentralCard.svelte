99ba6f18c802edc682a9a06c508e2a0f2582dd2b
nakko <langonebros2@gmail.com>
2024-01-17 16:58:50 +0100

    added socials

--- src/lib/CentralCard.svelte
+++ src/lib/CentralCard.svelte
@@ -13,6 +13,30 @@
             p: "Oltre a svolgere un ruolo di advocacy, la Consulta Provinciale degli Studenti di Firenze organizza anche eventi, incontri e campagne informative, al fine di sensibilizzare la comunità studentesca su questioni rilevanti e incoraggiare la partecipazione attiva. In questo modo, la Consulta diventa un punto di riferimento per gli studenti che desiderano essere coinvolti nella costruzione di un ambiente educativo più inclusivo, equo e partecipativo.",
         },
     ];
+
+    let logos = [
+        {
+            src: "logos/instagram.webp",
+            alt: "instagram logo",
+            url: "https://www.instagram.com/cps_firenze/"
+        },
+        {
+            src: "logos/gmail.webp",
+            alt: "gmail logo",
+            url: ""
+        },
+        {
+            src: "logos/consulta.jpeg",
+            alt: "website consulta",
+            url: "https://cms.csa.fi.it/Consulta/tabid/259/Default.aspx"
+        },
+        {
+            src: "logos/facebook.png",
+            alt: "facebook logo",
+            url: "https://www.facebook.com/cpsfi/?locale=it_IT"
+        }
+
+    ]
 </script>
 
 
@@ -23,7 +47,7 @@
         Consulta Provinciale Degli Studenti Firenze
     </p>
 
-    <div class="flex justify-center my-5 mx-2">
+    <div class="flex justify-center my-5 mx-2 max-w-5/6">
         <img
             src="images/palazzo-del-pegaso-via-cavour-firenze.jpg"
             alt="foto palazzo pegaso"
@@ -32,13 +56,21 @@
     </div>
     <div class="grid grid-cols-1 place-items-center mx-2 sm:mx-0">
         {#each paragraphs as { title, p }}
-            <div class="sm:w-5/6 text-center bg-[#e2e2e2] my-5 px-2 rounded-xl">
+            <div class="sm:w-5/6 text-center bg-[#e2e2e2] my-5 px-1 rounded-xl text-pretty">
                 <p class="text-2xl font-[600] text-start my-2">{title}</p>
                 <p>{p}</p>
             </div>
         {/each}
+
+        <div class="grid grid-cols-2 place-items-center gap-4 sm:w-5/6 mb-5">
+            {#each logos as {src, alt, url}}
+                <div class="flex justify-center bg-[#e2e2e2] rounded-xl w-full min-h-full">
+                    <a href="{url}" target="_blank" class="flex justify-center">
+                        <img src="{src}" alt="{alt}" class="w-1/4 my-5 object-contain">
+                    </a>
+                </div>
+            {/each}
+        </div>
     </div>
-    <div>
-        
-    </div>
+    
 </div>
